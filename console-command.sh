ac4cf077a310b9ccfe34d2c04f5d0b042ffa2827
Georgi Stoykov <georgi.v.stoykov@gmail.com>
2025-02-14 03:37:51 +0200

    test if gitignore is a problem

--- console-command.sh
+++
@@ -1,2 +0,0 @@
-# From /d:/SandBox/Task, run this command in the terminal:
-paste -d ":" <(grep -oP '<span class="strong">\s*\K[\d\.]+%(?=\s*</span>\s*<span class="quiet">)' coverage/index.html) <(grep -oP '(?<=<span class="quiet">)[^<]+' coverage/index.html)
